610a37f4252cb94104796c5a5f69032f2e98193a
Rohit Mondal <rohit.simbanic2023@gmail.com>
2023-06-09 12:59:01 +0530

    new htmlbutton feature added

--- src/Pages/CreatePost.tsx
+++ src/Pages/CreatePost.tsx
@@ -1,31 +1,9 @@
 import React, { useState } from "react";
 import ReactQuill, { Quill } from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import htmlEditButton from "quill-html-edit-button";
 
-// Custom module for embedding YouTube videos
-const EmbedVideo = Quill.import("formats/video");
-EmbedVideo.blotName = "embedVideo";
-EmbedVideo.className = "embed-video";
-Quill.register(EmbedVideo);
-
-const toolbarOptions = [
-  [{ font: [] }],
-  [{ header: [1, 2, 3, 4, 5, 6, false] }],
-  ["bold", "italic", "underline", "strike"],
-  [{ color: [] }, { background: [] }],
-  [{ script: "sub" }, { script: "super" }],
-  ["blockquote", "code-block"],
-  [{ list: "ordered" }, { list: "bullet" }],
-  [{ indent: "-1" }, { indent: "+1" }, { align: [] }],
-  ["link", "image", "video", "embedVideo"],
-  ["clean"],
-];
-
-const modules = {
-  toolbar: {
-    container: toolbarOptions,
-  },
-};
+Quill.register("modules/htmlEditButton", htmlEditButton);
 
 interface CreatePostProps {
   onSave: (content: string) => void;
@@ -43,17 +21,62 @@ const CreatePost: React.FC<CreatePostProps> = ({ onSave }) => {
     console.log(content);
   };
 
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }, { font: [] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image", "video"],
+      ["clean"],
+    ],
+    htmlEditButton: {
+      msg: "Edit the content in HTML format", //Custom message to display in the editor, default: Edit HTML here, when you click "OK" the quill editor's contents will be replaced
+      okText: "Ok", // Text to display in the OK button, default: Ok,
+      cancelText: "Cancel", // Text to display in the cancel button, default: Cancel
+      buttonHTML: "HTML", // Text to display in the toolbar button, default: <>
+      buttonTitle: "Show HTML source", // Text to display as the tooltip for the toolbar button, default: Show HTML source
+      syntax: false, // Show the HTML with syntax highlighting. Requires highlightjs on window.hljs (similar to Quill itself), default: false
+      prependSelector: "div#myelement", // a string used to select where you want to insert the overlayContainer, default: null (appends to body),
+      editorModules: {}, // The default mod
+    },
+  };
+  const formats = [
+    "header",
+    "font",
+    "size",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+    "video",
+  ];
+
   return (
     <div>
       <h1>Create a Post</h1>
-      <ReactQuill
-        value={content}
-        modules={modules}
-        onChange={handleEditorChange}
-        theme="snow"
-      />
+      <div className="desciption-container">
+        <ReactQuill
+          value={content}
+          formats={formats}
+          modules={modules}
+          onChange={handleEditorChange}
+          theme="snow"
+        ></ReactQuill>
+      </div>
+
       <button className="button" onClick={handleSave}>
-        Save
+        Create Post
       </button>
     </div>
   );
